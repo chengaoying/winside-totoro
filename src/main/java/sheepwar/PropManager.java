10d0c37be8e5acb2ed25340165be4a786034bf38
af <404755455@qq.com>
2012-11-06 11:24:20 +0800

    update

--- src/main/java/sheepwar/PropManager.java
+++ src/main/java/sheepwar/PropManager.java
@@ -67,7 +67,7 @@ public class PropManager implements Common{
 	
 		if (engine.getEngineService().getBalance() >= price) {
 			ServiceWrapper sw = engine.getServiceWrapper();
-			sw.purchaseProp(propId, propCount, "¹ºÂò"+propName);
+			sw.purchaseProp(propId, price, "¹ºÂò"+propName);
 			PopupText pt = UIResource.getInstance().buildDefaultPopupText();
 			if (sw.isServiceSuccessful()) {
 				pt.setText("¹ºÂò"+propName+"³É¹¦");
